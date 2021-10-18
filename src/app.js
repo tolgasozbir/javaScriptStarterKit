26d3971119e126f5cc5678316e3e8c36298fdbae
tolgasozbir <tolgasoz1@gmail.com>
2021-10-19 00:08:30 +0300

    js camp 18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,26 @@
+console.log("Merhaba Kodlama io")
+
+let UsdYesterday = 9.20
+let UsdToday = 9.30
+
+{
+    let UsdYesterday=9.10
+}
+
+console.log(UsdYesterday)
+
+const euroYesterday=11.2
+//euroYesterday=11
+
+console.log(euroYesterday)
+
+//Array
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+console.log(konutKredileri)
