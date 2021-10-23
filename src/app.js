66e813a4d4327346306d8f22ccec3bfb5063a192
tolgasozbir <tolgasoz1@gmail.com>
2021-10-23 22:19:14 +0300

    23.10.2021

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ console.log(euroYesterday)
 
 //Array
 let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+console.log(konutKredileri);
 
 console.log("<ul>")
 for (let i = 0; i < konutKredileri.length; i++) {
@@ -24,3 +25,58 @@ for (let i = 0; i < konutKredileri.length; i++) {
 console.log("</ul>")
 
 console.log(konutKredileri)
+
+//  object
+
+let student={id:1,name:"Tolga"};
+
+console.log(student.name);
+console.log(student);
+
+// functions
+
+function save(puan=10,ogrenci) {
+    console.log(ogrenci.name+ " : " +puan);
+}
+
+save(undefined,student);
+
+console.log("-------")
+
+//Rest
+//params c#
+//varArgs java
+
+let showProducts= function (id,...products) {
+    console.log(id);
+    console.log(products[0][1]);
+}
+
+showProducts(15,["Tolga","Levent","Bülent"],[1,2,3]);
+
+
+//Spread
+
+let values=[5,6,1,8,3,7,2];
+
+console.log(...values)
+console.log(Math.max(...values))
+console.log(..."ABC","D",..."EFG","H")
+
+//Destructuring
+
+let populations=[10000,20000,30000,[40000,50000]]
+let [small,medium,high,[veryHigh,maximum]] = populations
+console.log(small)
+console.log(medium)
+console.log(high)
+console.log(veryHigh)
+console.log(maximum)
+
+let product ={id:1,name:"Saat"}
+console.log(product.id)
+console.log(product["name"])
+
+let {id,name} = product
+console.log(id);
+console.log(name);
